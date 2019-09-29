e801c2088f9bd21df32036580f6ed07c9e07ea8b
MambetniyazovAmir <mr.amirniyaz@mail.ru>
2019-09-29 23:54:31 +0500

    minor changes

--- app/src/main/java/com/shagalalab/constitution/list/adapter/ItemViewHolder.kt
+++ app/src/main/java/com/shagalalab/constitution/list/adapter/ItemViewHolder.kt
@@ -1,7 +1,6 @@
 package com.shagalalab.constitution.list.adapter
 
 import android.view.View
-import android.widget.TextView
 import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
 import com.shagalalab.constitution.list.model.Item
@@ -9,13 +8,10 @@ import kotlinx.android.synthetic.main.item_view.view.*
 
 class ItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
-    var title: TextView = itemView.title_text
-    var description: TextView = itemView.description_text
-
     fun populateModel(model: Item, itemClickListener: ItemClickListener) {
-        title.text = model.title
-        description.text = model.description
-        description.isVisible = model.description.isNotEmpty()
+        itemView.title_text.text = model.title
+        itemView.description_text.text = model.description
+        itemView.description_text.isVisible = model.description.isNotEmpty()
         itemView.setOnClickListener {
             itemClickListener.onItemClick(model)
         }
