366cc5503cddafdd8a203319a43f56ac4c788df6
MambetniyazovAmir <mr.amirniyaz@mail.ru>
2020-01-11 20:00:45 +0500

    simplified fragment_article

--- app/src/main/java/com/shagalalab/constitution/article/ArticleFragment.kt
+++ app/src/main/java/com/shagalalab/constitution/article/ArticleFragment.kt
@@ -1,23 +1,26 @@
 package com.shagalalab.constitution.article
 
+import android.graphics.Typeface
 import android.os.Bundle
+import android.text.SpannableStringBuilder
+import android.text.Spanned
+import android.text.method.ScrollingMovementMethod
+import android.text.style.StyleSpan
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
-import androidx.recyclerview.widget.DividerItemDecoration
-import com.shagalalab.constitution.R
-import com.shagalalab.constitution.article.adapter.ArticleAdapter
 import com.shagalalab.constitution.data.ConstitutionDatabase
 import kotlinx.android.synthetic.main.fragment_article.*
 
-class ArticleFragment(private var chapterId: Int) : Fragment(R.layout.fragment_article) {
+
+class ArticleFragment(private var chapterId: Int) :
+    Fragment(com.shagalalab.constitution.R.layout.fragment_article) {
 
     companion object {
         const val TAG = "ArticleFragment"
     }
 
     lateinit var viewModel: ArticleViewModel
-    private val adapter = ArticleAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,16 +30,18 @@ class ArticleFragment(private var chapterId: Int) : Fragment(R.layout.fragment_a
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        list_article.adapter = adapter
-        list_article.addItemDecoration(
-            DividerItemDecoration(
-                context,
-                DividerItemDecoration.VERTICAL
-            )
-        )
         viewModel.getArticles(chapterId)
-        viewModel.articleList.observe(this, Observer {
-            adapter.setData(it)
+        viewModel.articleList.observe(this, Observer { list ->
+            val s = SpannableStringBuilder()
+            list.forEach {
+                val st = StyleSpan(Typeface.BOLD)
+                s.append(it.title, st, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+                s.append(". ")
+                s.append(it.description)
+                s.append("\n")
+            }
+            textView.text = s
+            textView.movementMethod = ScrollingMovementMethod()
         })
     }
 }
