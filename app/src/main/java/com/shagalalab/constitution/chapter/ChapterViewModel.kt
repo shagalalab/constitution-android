c0e4b936376cebdfd37baa7c1be4b6d7fbad5c63
MambetniyazovAmir <mr.amirniyaz@mail.ru>
2020-04-05 18:33:42 +0500

    minor changes

--- app/src/main/java/com/shagalalab/constitution/chapter/ChapterViewModel.kt
+++ app/src/main/java/com/shagalalab/constitution/chapter/ChapterViewModel.kt
@@ -1,6 +1,5 @@
 package com.shagalalab.constitution.chapter
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -12,10 +11,6 @@ class ChapterViewModel(private val chapterDao: ChapterDao) : ViewModel() {
 
     private val chapterListLiveData: MutableLiveData<List<ChapterModel>> = MutableLiveData()
     val chapterList: LiveData<List<ChapterModel>> = chapterListLiveData
-
-    init {
-        Log.i("ChapterViewModel", "ChapterViewModel created!")
-    }
 
     fun getChaptersByPartId(partId: Int) {
         Executors.newSingleThreadExecutor().execute {
